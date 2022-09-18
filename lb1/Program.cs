878d62f26d91d269b51d6dfa0b859d0c6b801f1f
axe6ao <axe7ao@gmail.com>
2022-09-18 22:22:25 +0300

    Lab2

---
+++ lb1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace lb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Apartment = "ds";
+            address.Street = "ds";
+            address.House = "ds";
+            address.City = "ds";
+            address.Country = "ds";
+            address.Index = 1;
+
+            foreach (PropertyInfo property in address.GetType().GetProperties())
+            {
+                Console.WriteLine(property.Name + " : " + property.GetValue(address, null));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
