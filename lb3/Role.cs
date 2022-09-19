c648c6f552d19da0a822f821dfe098c34e5b87d9
axe6ao <axe7ao@gmail.com>
2022-09-19 08:55:32 +0300

    Added 2 last progs

---
+++ lb3/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb3
+{
+    public enum Role
+    {
+        Junior = 0,
+        Middle = 1,
+        Senior = 2,
+    }
+}
---
+++ lb4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace lb4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            user.Surname = "Adamson";
+            user.Name = "Adam";
+            user.Login = "adam";
+            user.Age = 27;
+
+            foreach (PropertyInfo property in user.GetType().GetProperties())
+            {
+                Console.WriteLine(property.Name + " : " + property.GetValue(user, null));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
